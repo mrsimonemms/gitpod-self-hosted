6ce59ccdb1b8119c4a1b7c1aae848a716078826b
Simon Emms <simon@simonemms.com>
2023-02-19 21:24:03 +0000

    fix(k3s): output the k3s token from the providers

--- infrastructure/providers/hetzner/output.tf
+++ infrastructure/providers/hetzner/output.tf
@@ -21,6 +21,12 @@ output "dns_records" {
   ]
 }
 
+output "k3s_token" {
+  description = "Join token for k3s"
+  value       = module.k3s_setup.k3s_token
+  sensitive   = true
+}
+
 output "kubeconfig" {
   description = "Kubernetes config YAML file"
   value       = module.k3s_setup.kubeconfig
